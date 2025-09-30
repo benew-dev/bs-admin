9760c18f11378bc7a19c307da9ba17fc1b3c6b3a
Fathismael93 <fathismael@gmail.com>
2025-09-30 20:30:17 +0300

    Handled adding product issue

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,7 +6,10 @@ const isAuthenticatedUser = async (req, res) => {
   const session = await getServerSession(auth);
 
   if (!session) {
-    return res.error("Login first to access this route", 401);
+    return res.json(
+      { success: false, message: "Login first to access this route" },
+      { status: 401 },
+    );
   }
 
   req.user = session.user;
@@ -17,9 +20,12 @@ const authorizeRoles = (res, ...roles) => {
     const user = await User.findOne({ email: req.user.email }).select("role");
 
     if (!roles.includes(user.role)) {
-      return res.error(
-        `Role (${user.role}) is not allowed to access this resource.`,
-        401,
+      return res.json(
+        {
+          success: false,
+          message: `Role (${user.role}) is not allowed to access this resource.`,
+        },
+        { status: 401 },
       );
     }
   };
