07e7dbf81874d8c3d64ae88c46057000f2531d73
Fathismael93 <fathismael@gmail.com>
2025-09-30 12:47:58 +0300

    Added files for auth, products and settings

--- app/admin/settings/page.jsx
+++ app/admin/settings/page.jsx
@@ -1,33 +1,25 @@
-import React from 'react';
-import dynamic from 'next/dynamic';
-import Loading from '@/app/loading';
+import React from "react";
+import dynamic from "next/dynamic";
+import Loading from "@/app/loading";
 
-const Settings = dynamic(() => import('@/components/settings/Settings'), {
+const Settings = dynamic(() => import("@/components/settings/Settings"), {
   loading: () => <Loading />,
 });
 
 import {
   getCategoryData,
-  getDeliveryPrice,
   getPaymentTypeData,
-} from '@/backend/utils/server-only-methods';
+} from "@/backend/utils/server-only-methods";
 
 export const metadata = {
-  title: 'Dashboard - Settings',
+  title: "Dashboard - Settings",
 };
 
 const SettingsPage = async () => {
-  const deliveryPriceData = await getDeliveryPrice();
   const categoryData = await getCategoryData();
   const paymentTypeData = await getPaymentTypeData();
 
-  return (
-    <Settings
-      dataCategory={categoryData}
-      dataPayment={paymentTypeData}
-      dataDeliveryPrice={deliveryPriceData}
-    />
-  );
+  return <Settings dataCategory={categoryData} dataPayment={paymentTypeData} />;
 };
 
 export default SettingsPage;
